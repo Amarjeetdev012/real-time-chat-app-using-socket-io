7f1e5afce25e96f226bbd8d587b7e49c98d15ec3
Amarjeetdev012 <117710031+Amarjeetdev012@users.noreply.github.com>
2023-01-27 10:22:05 +0530

    update room

--- controller/room.controller.js
+++ controller/room.controller.js
@@ -6,25 +6,29 @@ dotenv.config();
 const secret = process.env.JWTSECRET;
 
 export const redirectRoom = async (req, res) => {
-  const token = req.cookies.token;
-  const room = req.query.rooms;
-  if (!token) {
-    return res.status(400).send({
-      status: false,
-      msg: 'your session has expired please login again',
-    });
+  try {
+    const token = req.cookies.token;
+    const room = req.query.rooms;
+    if (!token) {
+      return res.status(400).send({
+        status: false,
+        msg: 'your session has expired please login again',
+      });
+    }
+    const data = jwt.verify(token, secret);
+    const checkId = await findId(data._id);
+    const username = checkId.username;
+    if (!checkId) {
+      return res.status(401).send({
+        status: false,
+        message: 'you are not a authorised person',
+      });
+    }
+    const data = {}
+    data.username = username
+    data.room = room
+    res.status(200).send({ status: true, msg='you have entered in room succesfully', data: data })
+  } catch (error) {
+    return res.status(500).send({ status: false, msg: err.message });
   }
-  const data = jwt.verify(token, secret);
-  const checkId = await findId(data._id);
-  const username = checkId.username;
-  if (!checkId) {
-    return res.status(401).send({
-      status: false,
-      message: 'you are not a authorised person',
-    });
-  }
-  res.render(path.join(__dirname, '/', 'rooms.ejx'), {
-    room: room,
-    name: username,
-  });
-};
+}
