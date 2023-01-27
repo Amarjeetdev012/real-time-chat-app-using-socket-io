cf71f6fa0696e5591863945bbd0b0231fdd61e9d
Amarjeetdev012 <117710031+Amarjeetdev012@users.noreply.github.com>
2023-01-27 12:14:06 +0530

    update

--- utils/user.utils.js
+++ utils/user.utils.js
@@ -4,6 +4,7 @@ const users = [];
 export const userJoin = (id, username, room) => {
   const user = { id, username, room };
   users.push(user);
+  console.log('userJoin', users)
   return user;
 };
 
@@ -15,6 +16,7 @@ export const getCurrentUser = (id) => {
 // User leaves chat
 export const userLeave = (id) => {
   const index = users.findIndex((user) => user.id === id);
+  console.log('userLeave index', index);
   if (index !== -1) {
     return users.splice(index, 1)[0];
   }
