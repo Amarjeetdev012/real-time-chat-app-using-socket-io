e0c308862a808bbcdd881d322eab94cba92d3133
unknown <amarjeet@baruzotech.com>
2023-01-30 12:53:33 +0530

    updaterooms

---
+++ models/room.model.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const roomSchema = new mongoose.Schema({
+  roomName: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  userName: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
+
+const Room = mongoose.model('rooms', roomSchema);
+
+export const createRoom = async (roomName, username) => {
+  const data = {};
+  data.roomName = roomName;
+  data.username = username;
+  await Room.create(data);
+  return data;
+};
+
+export const allRooms = async () => {
+  return await Room.find();
+};
